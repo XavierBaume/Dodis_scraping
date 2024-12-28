30c3a72cda028cb2d278e10eebd464b9fb29d417
XavierBaume <145542531+XavierBaume@users.noreply.github.com>
2024-12-28 11:52:39 +0100

    Update DD-MM-YYYY_to_YYYY-MM-DD.py

--- DD-MM-YYYY_to_YYYY-MM-DD.py
+++ DD-MM-YYYY_to_YYYY-MM-DD.py
@@ -6,7 +6,7 @@ data = pd.read_csv(file_path)
 # Convertir les dates dans la colonne 'dates_document_date' du format DD-MM-YYYY à YYYY-MM-DD
 data['dates_document_date'] = pd.to_datetime(data['dates_document_date'], format='%d-%m-%Y', errors='coerce').dt.strftime('%Y-%m-%d')
 
-output_path = 'bis_dodis_dataBaseRendu_transformed_final.csv'  # Remplacez par le chemin de sortie souhaité
+output_path = 'bis_dodis_dataBaseRendu_transformed_final.csv'
 data.to_csv(output_path, index=False)
 
 print(f"Fichier modifié sauvegardé à : {output_path}")
